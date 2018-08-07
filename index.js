b9f83f38bcf669213ab732356f4c6f4e7e0fe591
Penny Cornell <penny.cornell1@hotmail.com>
2018-08-07 12:20:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ var recipes = {}
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, {[key]: value})
   
-  updateObjectWithKeyAndValue(recipes, 'chocolate', '1 cup')
+  
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value
   return object
   
-  destructivelyUpdateObjectWithKeyAndValue(recipe, 'flour', '3 cups')
+  
 }
 function deleteFromObjectByKey(object, key){
   const newObj = Object.assign({}, object)
