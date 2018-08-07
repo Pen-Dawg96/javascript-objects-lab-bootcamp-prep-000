cf69fffa6c8fbd1d1ec809ebc74e387d0553e9e4
Penny Cornell <penny.cornell1@hotmail.com>
2018-08-07 10:59:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ var recipes = { eggs: 3}
 
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, {[key]: value})
+  
   updateObjectWithKeyAndValue(recipes, 'chocolate', '1 cup')
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   obect[key] = value
+  return object
+  
+  
 }
