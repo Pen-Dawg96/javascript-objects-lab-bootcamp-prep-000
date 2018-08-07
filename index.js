50dc428cf66dbc4897e7bbeacc8ba681b5370b42
Penny Cornell <penny.cornell1@hotmail.com>
2018-08-07 12:02:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ function deleteFromObjectByKey(object, key){
 function destructivelyDeleteFromObjectByKey(object, key){
  var obj = { eggs: 3} 
  var newObj = (obj, 'milk')
- return Object.assign({})
+ return Object.assign({}, obj, flour)
 }
